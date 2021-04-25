2242bf25ce37791dd1aab961923ff3b5ee710206
Jason9789 <lifebook0809@gmail.com>
2021-04-25 19:41:44 +0900

    feat: Add tabBar

---
+++ 2AM/2AM/View/MyPage/MyPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageViewcontroller.swift
+//  2AM
+//
+//  Created by 전판근 on 2021/04/25.
+//
+
+import UIKit
+import SnapKit
+
+class MyPageViewController: UIViewController {
+    override func viewDidLoad() {
+        print("hi")
+    }
+}
+
