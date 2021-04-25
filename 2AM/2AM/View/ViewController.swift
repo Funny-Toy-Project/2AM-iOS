ec04dc6598cf89344980f34bec98e30a62c4289e
Jason9789 <lifebook0809@gmail.com>
2021-04-25 19:15:32 +0900

    feat: Add RxSwift, RxCocoa, SnapKit

---
+++ 2AM/2AM/View/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  2AM
+//
+//  Created by 전판근 on 2021/04/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    private let myView: UIButton = {
+        let v = UIButton()
+        v.setTitle("HI", for: .normal)
+        v.setTitleColor(.white, for: .normal)
+        v.backgroundColor = .blue
+        return v
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("HI")
+        configureView()
+        configureSubView()
+    }
+    
+    func configureView() {
+        view.backgroundColor = .white
+        
+        view.addSubview(myView)
+        myView.snp.makeConstraints {
+            $0.leading.equalTo(view.snp.leading).offset(100)
+            $0.top.equalTo(view.snp.top).offset(100)
+        }
+        
+    }
+    
+    func configureSubView() {
+        
+    }
+
+
+}
+
