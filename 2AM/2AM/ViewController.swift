31fb925f7effac47a4f5cfe63a9a85159ab8d71f
Jason9789 <lifebook0809@gmail.com>
2021-04-25 19:04:56 +0900

    init: Projecdt init

---
+++ 2AM/2AM/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  2AM
+//
+//  Created by 전판근 on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        print("HIHIHI")
+    }
+
+
+}
+
