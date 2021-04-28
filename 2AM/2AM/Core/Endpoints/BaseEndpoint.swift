de223ce412e24e9292645f38c727dac2a3c6ad1d
Jason9789 <lifebook0809@gmail.com>
2021-04-28 23:56:56 +0900

    feat: Add Endpoint

---
+++ 2AM/2AM/Core/Endpoints/BaseEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  BaseEndpoint.swift
+//  2AM
+//
+//  Created by 전판근 on 2021/04/28.
+//
+
+import Foundation
---
+++ 2AM/2AM/Core/Endpoints/UserEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  UserEndpoint.swift
+//  2AM
+//
+//  Created by 전판근 on 2021/04/28.
+//
+
+import Foundation
